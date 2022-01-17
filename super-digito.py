9487a79ec897168d4154cded0141ed0fd4f8a193
Victoria Nedina Nunes <victoria-nedina@hotmail.com>
2022-01-17 19:38:17 -0300

    Adição de questão super-digito

--- super-digito.py
+++ super-digito.py
@@ -2,10 +2,11 @@ def super_digito (numero, fator):
     novo_numero = int(str(numero)*fator)
     lista = [int(num) for num in str(novo_numero)]
 
-    
+
     if len(lista) == 1:
         return lista
     else:
-        return super(sum(lista),1)
+        return super_digito(sum(lista),1)
 
-super_digito(123,3)
+
+super_digito(123, 3)
